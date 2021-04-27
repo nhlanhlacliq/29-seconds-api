677999ae3642ee1ef98caa349e62ba16fc6c481c
nhlanhlacliq <nhlanhlacliq@gmail.com>
2021-04-27 21:06:54 +0200

    added old generate wordcloud function

--- model.py
+++ model.py
@@ -10,6 +10,12 @@ https://flask-sqlalchemy.palletsprojects.com/.
 """
 import json
 from random import randint
+import matplotlib.pyplot as plt
+from os import sys
+from nltk.probability import FreqDist
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from wordcloud import WordCloud, STOPWORDS
 
 def db(category, difficulty):
     # get category, question, answer 
@@ -36,8 +42,62 @@ def load_db(category, difficulty):
 #     with open('flashcards_db.json', 'w') as f:
 #         return json.dump(db, f)
 
+"""Generates and shows a wordcloud from a chosen category. 
+  The summary contains the synopsis which the WC is generated from"""
 def generate_image(question):
+    # get time limit and difficulty level from difficulty object
+    time_limit = difficulty_object.get_time_limit()
+    difficulty_level = difficulty_object.get_difficulty()
 
+    # get category and summary from summary object
+    category = summary_object.get_category()
+    summary = summary_object.get_summary()
+
+    # stop words ("and", "the", "we", etc.)
+    stops = set(stopwords.words('english')) #Set used for speed
+    more_stops= STOPWORDS
+
+    # will need this for plotting too
+    # Make list of words in the summary. dont add word if its a stop word..
+    words_in_summary = [word for word in word_tokenize(summary) if ((word not in stops) and (word not in more_stops))]
+    words_freq_dist = FreqDist(words_in_summary)
+
+    # remove x = {difficulty level} most repeated words, add to clues list
+    # clues not yet working.. returns int of word counts instead of actual word
+    clues = []
+    for i in range(difficulty_level):
+      clue = words_freq_dist.pop(words_freq_dist.max())
+      clues.append(clue)
+      
+    # print(f"These would be the clues {clues}")
+    # adjusted summary is summary without x most repeated words 
+    adjusted_summary = ''
+    for word in words_freq_dist.keys():
+      adjusted_summary += word + ' '
+    # generate wordcloud from adjusted summary
+    wc_rand_state = random.randint(7, 9)
+    wc = WordCloud(max_words=500,relative_scaling=0.5,
+                  background_color='black',stopwords=more_stops,
+                  margin=2,random_state=wc_rand_state,contour_width=0.5,
+                  contour_color='white', colormap='Accent')
+    wc.generate(adjusted_summary)
+    colors = wc.to_array()
+    # plotting frequency distribution of words in summary synopsis.. not really needed. I just wanted to see..
+    # words_freq_dist.plot(15, linestyle='-', title="LOL words")
+    # plt.legend()
+
+    # show wordcloud 
+    plt.ion()
+    plt.figure()
+    plt.title(f"Which {category} is this?\n", fontsize=20, color='black')
+    plt.imshow(colors, interpolation="bilinear")
+    plt.axis('off')
+    plt.show()
+    plt.pause(0.001)
+    # countdown timer
+    self.countdown_timer(time_limit)
+    plt.close(1)
+    print("Time's up!")
 
 
     
