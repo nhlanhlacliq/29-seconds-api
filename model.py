934c84525d5d49d7fd2acbce1ab3104ff4ef65a8
nhlanhlacliq <nhlanhlacliq@gmail.com>
2021-05-10 13:30:19 +0200

    refactor

--- model.py
+++ model.py
@@ -15,7 +15,7 @@ client = MongoClient("mongodb+srv://admin:octopus@29seconds.s8flw.mongodb.net/my
 db=client["anime_db"]
 
 # return random entry from database
-def read_random(category, difficulty, url_root):
+def read_random(category, difficulty_lvl, page_url):
     database = []
     collection=db[category]
     results = collection.find()
@@ -23,23 +23,23 @@ def read_random(category, difficulty, url_root):
         database.append(result)
     # choose random selection from data
     shuffle(database)
-    data_random = database[randint(0,len(database)-1)]
-    data_random["category"] = category
-    data_random["choices"] = add_choices(data_random)
-    data_random["image"] = generate_image(data_random, difficulty, url_root)
-    return jsonify(json.loads(json_util.dumps(data_random)))
+    data_object = database[randint(0,len(database)-1)]
+    data_object["category"] = category
+    data_object["choices"] = add_choices(data_object)
+    data_object["image"] = generate_image(data_object, difficulty_lvl, page_url)
+    return jsonify(json.loads(json_util.dumps(data_object)))
 
 # insert new data to mongo db
 def create(category, answer, question):
-    data = {"question":question,
+    data_object = {"question":question,
             "answer":answer}
     # if it already exists, exit function. else insert into db.
     collection=db[category]
-    results = collection.find(data)
+    results = collection.find(data_object)
     for result in results:
         if result["answer"] == answer:
             return
-    return collection.insert_one(data)        
+    return collection.insert_one(data_object)        
 
 # Return data from mongo db
 def read_all(categories):
@@ -51,22 +51,22 @@ def read_all(categories):
             database.append(result)
     return jsonify(json.loads(json_util.dumps(database)))
 
-# Generates and stores a wordcloud from question. return image url
-def generate_image(data, difficulty, url_root):
-    answer = data["answer"]
-    question = data["question"]
-    category = data["category"]
-    difficulty = int(difficulty)
+# Generates and store wordcloud image from question. returns image url
+def generate_image(data_object, difficulty_lvl, page_url):
+    answer = data_object["answer"]
+    question = data_object["question"]
+    category = data_object["category"]
+    difficulty_lvl = int(difficulty_lvl)
 
-    # Make list of words in the question. dont add word if its a stop word..
+    # Make list of words in the question excluding stop words.
     words_in_question = [word for word in word_tokenize(question) if word not in STOPWORDS]
     words_freq_dist = FreqDist(words_in_question)
 
-    # remove x = {difficulty level} most repeated words, add to clues list
-    for i in range(difficulty):
+    # remove x = {difficulty_lvl level} most repeated words, add to clues list
+    for i in range(difficulty_lvl):
         words_freq_dist.pop(words_freq_dist.max())
       
-    # adjusted question is question without x most repeated words 
+    # create string without x most repeated words 
     adjusted_question = ''
     for word in words_freq_dist.keys():
         adjusted_question += word + ' '
@@ -90,20 +90,20 @@ def generate_image(data, difficulty, url_root):
     plt.imshow(colors, interpolation="bilinear")
     plt.axis('off')
 
-    filename = f"./static/{answer}{difficulty}.png"
+    filename = f"./static/{answer}{difficulty_lvl}.png"
     filename = filename.replace(" ","")
     # save image else return existing one if it already exists
     if not os.path.exists(filename):
         plt.savefig(filename, dpi=300, bbox_inches='tight')
-    url = url_root + filename[2:]
+    url = page_url + filename[2:]
 
     return url
 
 # Add other answers within same category
-def add_choices(data):
+def add_choices(data_object):
     choices = []
     database = []
-    collection=db[data["category"]]
+    collection=db[data_object["category"]]
     results = collection.find()
     for result in results:
         database.append(result["answer"])
