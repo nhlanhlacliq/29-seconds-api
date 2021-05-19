ef20421095dfce80def31645bb5765c6283eebf3
nhlanhlacliq <nhlanhlacliq@gmail.com>
2021-05-19 10:30:42 +0200

    created cors anywhere api from github

--- OLD/response_test.js
+++ OLD/response_test.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch');	//npm install node-fetch
 
 
 // GET Request.
-fetch('https://api-29-seconds.herokuapp.com/api', {method: "POST", headers: {"difficulty": "1", "category": "anime"}})
+fetch('https://blooming-savannah-44967.herokuapp.com/https://api-29-seconds.herokuapp.com/api', {method: "POST", headers: {"difficulty": "1", "category": "anime"}})
     // Handle success
     .then(response => response.json())  // convert to json
     .then(json => console.log(json))    //print data to console
