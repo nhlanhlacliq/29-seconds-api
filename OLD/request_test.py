6a851e536294fb700a9c4bee2d4ffffb35d4f91b
nhlanhlacliq <nhlanhlacliq@gmail.com>
2021-06-08 21:36:12 +0200

    remove test

--- OLD/request_test.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-data = {"difficulty": "1", "category": "anime"}
-url = "https://api-29-seconds.herokuapp.com/api/https://api-29-seconds.herokuapp.com/api"
-response = requests.post(url, data)
-print(response.text)
